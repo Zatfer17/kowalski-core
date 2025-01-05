c83344251932b875aba434254ba45c5289bffbbe
Matteo Ferrini <mimmi.ferrini@gmail.com>
2025-01-05 10:54:23 +0100

    Remove gitJournal fix for now

--- src/kowalski_core/cmd/list.py
+++ src/kowalski_core/cmd/list.py
@@ -2,6 +2,7 @@ from glob                          import glob
 from os                            import path
 from kowalski_core.internal.config import KOWALSKI_PATH
 from frontmatter                   import load
+from datetime                      import datetime
 from kowalski_core.internal.note   import Note
 
 
@@ -10,9 +11,12 @@ def listCmd():
     notes_paths = glob(path.join(KOWALSKI_PATH, "*.md"))
 
     notes = []
+
     for p in notes_paths:
+
         note_md = load(p)
-        note = Note(note_md['id'], note_md['created'], note_md['modified'], note_md.content)
+
+        note = Note(id, note_md['created'], note_md['modified'], note_md.content)
         notes.append(note.id)
 
         print(note.description('short'))
