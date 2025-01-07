a256340a23a019888b72e49c107689b313e29a22
Matteo Ferrini <mimmi.ferrini@gmail.com>
2025-01-07 21:39:15 +0100

    patch: Fix typo

--- src/kowalski/cmd/list.py
+++ src/kowalski/cmd/list.py
@@ -6,7 +6,7 @@ from kowalski.internal.note   import Note
 
 
 def listCmd(keyword: str, limit: int):
-    if keyword is "":
+    if keyword == "":
         notes_paths = glob(f"{KOWALSKI_PATH}/*.md")
     else:
         if " " in keyword:
