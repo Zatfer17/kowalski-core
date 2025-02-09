7f6487897851809adc5c272f08a02ed0cee785ff
Matteo Ferrini <mimmi.ferrini@gmail.com>
2025-02-09 11:13:57 +0100

    Update note print

--- internal/model/note.go
+++ internal/model/note.go
@@ -24,16 +24,38 @@ func (note Note) GetName() string {
 	return fmt.Sprintf("%s.md", note.Created)
 }
 
+func Colored(r, g, b int, text string) string {
+	return fmt.Sprintf("\033[38;2;%d;%d;%dm%s\033[0m", r, g, b, text)
+}
+
+func Bold(text string) string {
+	return fmt.Sprintf("\033[1m%s\033[0m", text)
+}
+
 func (note Note) String() string {
 	
 	name := note.GetName()
+	name  = Colored(235, 171, 52, name)
+	name  = Bold(name)
 	
 	content := note.Content
-	content =  strings.ReplaceAll(content, "\n", " ")
+	content  = strings.ReplaceAll(content, "\n", " ")
+	if len(content) > 27 {
+		content = content[:27]
+	}
+	content = content + strings.Repeat(".", 30-len(content))
+	content = Bold(content)
 
-	tags := note.Tags
+	tags := strings.Join(note.Tags, ", ")
+	tags  = strings.ReplaceAll(tags, "[", "")
+	tags  = strings.ReplaceAll(tags, "]", "")
+	if len(tags) > 17 {
+		tags = tags[:17]
+	}
+	tags = tags + strings.Repeat(".", 20-len(tags))
+	tags = Colored(0, 168, 138, tags)
 
-	return fmt.Sprintf("(%s): [%s] %s", name, content, tags)
+	return fmt.Sprintf("(%s): [🗞  %s] [🏷️  %s]", name, content, tags)
 }
 
 func (note Note) Format() string {
